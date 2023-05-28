c6565e81fae26f6cbdc8096af7deb5bf7be7b4b7
RyanSka1 <109309808+RyanSka1@users.noreply.github.com>
2023-05-28 18:56:02 -0300

    Arrumando login

--- Php/login.php
+++ Php/login.php
@@ -1,10 +1,11 @@
 <?php
 include 'db_conn.php';
+session_start(); // Inicia a sessão
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$sql = "SELECT password FROM users WHERE username = ?";
+$sql = "SELECT id, username, password FROM users WHERE username = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("s", $username);
 $stmt->execute();
@@ -13,6 +14,9 @@ $result = $stmt->get_result();
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
     if (password_verify($password, $row['password'])) {
+        // Armazena os dados do usuário na sessão
+        $_SESSION['user_id'] = $row['id'];
+        $_SESSION['username'] = $row['username'];
         echo "Login efetuado com sucesso!";
     } else {
         echo "Senha incorreta!";
--- Php/logout.php
+++ Php/logout.php
@@ -1,7 +1,11 @@
 <?php
+
+include 'db_conn.php';
+
 // logout.php
 session_start();
-if(session_destroy()) {
-    header("Location: login.php");
-}
+session_unset();
+session_destroy();
+header("Location: login.php");
+exit;
 ?>
