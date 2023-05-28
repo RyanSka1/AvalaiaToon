ffec2f8278076d0360eba7c0da9fda61674bbee2
RyanSka1 <109309808+RyanSka1@users.noreply.github.com>
2023-05-28 19:24:10 -0300

    Adicionando email de verificação

--- MySQL/AvalaiaToon.sql
+++ MySQL/AvalaiaToon.sql
@@ -10,11 +10,24 @@ CREATE TABLE users (
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL, -- Armazene as senhas como hashes
     email VARCHAR(100) NOT NULL,
-    cpf CHAR(14) NOT NULL,
-    phone CHAR(15) NOT NULL
     ALTER TABLE users ADD COLUMN verified BOOLEAN DEFAULT 0;
-
+    verified BOOLEAN DEFAULT 0
 );
+
+CREATE TABLE email_verification_tokens (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    token VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE email_verification_tokens ADD FOREIGN KEY (user_id) REFERENCES users(id);
+
+CREATE EVENT IF NOT EXISTS delete_expired_tokens
+ON SCHEDULE EVERY 1 MINUTE
+DO
+    DELETE FROM email_verification_tokens WHERE created_at < DATE_SUB(NOW(), INTERVAL 24 HOUR);
+
 
 CREATE TABLE password_resets (
     id INT AUTO_INCREMENT PRIMARY KEY,
--- Php/reset_password.php
+++ Php/reset_password.php
@@ -1,16 +1,32 @@
 <?php
-
 include 'db_conn.php';
 
 $email = $_POST['email'];
 
-$sql = "SELECT email FROM users WHERE email = ?";
+$sql = "SELECT id FROM users WHERE email = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("s", $email);
 $stmt->execute();
 
 $result = $stmt->get_result();
 if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $user_id = $row['id'];
+
+    // Gera um token único
+    $token = bin2hex(random_bytes(50));
+
+    // Insere o token no banco de dados
+    $sql = "INSERT INTO password_resets (user_id, token) VALUES (?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("is", $user_id, $token);
+    $stmt->execute();
+
+    // Envia um e-mail para o usuário com o link de redefinição de senha
+    $reset_link = "http://yourwebsite.com/reset_password.php?token=$token";
+    $message = "Clique no seguinte link para redefinir sua senha: $reset_link";
+    mail($email, "Redefinição de senha", $message);
+
     echo "Um link de redefinição de senha foi enviado para o seu email!";
 } else {
     echo "E-mail não encontrado!";
